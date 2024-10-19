0d34e2ac059770eb928f579db3db1ac67208567f
Justin Lam <justin.lam@uplight.com>
2024-10-19 01:04:12 -0700

    Update final sushi message

--- js/questions/sushi.js
+++ js/questions/sushi.js
@@ -1,20 +1,23 @@
 const sushi_team1 = [
   {
     question: "Sushi go points for player 1",
-    answer: "18"
+    answer: "18",
+    message: "Congrats! See Justin for your next challenge and get ready to <a href='https://www.instagram.com/p/C2-3cqSSb-r/'>FLY LIKE A BIRD!</a>", 
   }
 ]
 
 const sushi_team2 = [
   {
     question: "Sushi go points for player 2",
-    answer: "15"
+    answer: "15",
+    message: "Congrats! See Justin for your next challenge and get ready to <a href='https://www.instagram.com/p/C2-3cqSSb-r/'>FLY LIKE A BIRD!</a>", 
   }
 ]
 
 const sushi_team3 = [
   {
     question: "Sushi go points for player 3",
-    answer: "16"
+    answer: "16",
+    message: "Congrats! See Justin for your next challenge and get ready to <a href='https://www.instagram.com/p/C2-3cqSSb-r/'>FLY LIKE A BIRD!</a>", 
   }
 ]
