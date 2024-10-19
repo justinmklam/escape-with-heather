d949e0665a9db554c4464dbb548520f8853cb931
Justin Lam <justin.lam@uplight.com>
2024-10-18 17:24:46 -0700

    Add show/hide password button

--- js/login.js
+++ js/login.js
@@ -15,3 +15,15 @@ function checkPassword() {
   }
 }
 
+function togglePassword() {
+  const passwordInput = document.getElementById('password');
+  const toggleButton = document.getElementById('togglePassword');
+
+  if (passwordInput.type === 'password') {
+    passwordInput.type = 'text';
+    toggleButton.textContent = 'Hide';
+  } else {
+    passwordInput.type = 'password';
+    toggleButton.textContent = 'Show';
+  }
+}
