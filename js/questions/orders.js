b91f2c0115e00c3dd7b3f495e55794938137e192
Justin Lam <justin.lam@uplight.com>
2024-10-18 22:14:38 -0700

    Fix order link

--- js/questions/orders.js
+++ js/questions/orders.js
@@ -8,7 +8,7 @@ const orders_team1 = [
       "__ plum tomatoes\n" + // 5
       "__ g dark musovado sugar", // 100
     answer: "4215100",
-    message: "Congrats! Click <a href='?questionprefix=durians_all&team=team1'>here</a> to get your promo code!",
+    message: "Congrats! Click <a href='/escape-with-heather/?questionprefix=durians_all&team=team1'>here</a> to get your promo code!",
   },
 ]
 
@@ -22,7 +22,7 @@ const orders_team2 = [
       "__ cinnamon stick\n" + // 1
       "__ g dark-roast ground coffee", // 35
     answer: "4112135",
-    message: "Congrats! Click <a href='?questionprefix=durians_all&team=team2'>here</a> to get your promo code!",
+    message: "Congrats! Click <a href='/escape-with-heather/?questionprefix=durians_all&team=team2'>here</a> to get your promo code!",
   },
 ]
 
@@ -36,6 +36,6 @@ const orders_team3 = [
       "__ chillies\n" + // 3
       "__ rice wrappers", // 10
     answer: "423310",
-    message: "Congrats! Click <a href='?questionprefix=durians_all&team=team3'>here</a> to get your promo code!",
+    message: "Congrats! Click <a href='/escape-with-heather/?questionprefix=durians_all&team=team3'>here</a> to get your promo code!",
   },
 ]
