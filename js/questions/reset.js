cca937186c4bcb652704c54a70f0127b387613d2
Justin Lam <justin.lam@uplight.com>
2024-10-19 00:26:32 -0700

    Add password reset questions

---
+++ js/questions/reset.js
@@ -0,0 +1,47 @@
+const reset_team1 = [
+  {
+    question: "Password reset security question 1:\n\nWhat is your favourite fruit?",
+    answer: "durian"
+  },
+  {
+    question: "Password reset security question 2:\n\nWhat date were you born?",
+    answer: "17"
+  },
+  {
+    question: "Password reset security question 3:\n\nWhat do you like to *drink* out of cups??",
+    answer: "water",
+    message: "Part 1/3 of password has been reset: climberg1rl-***-***",
+  }
+]
+
+const reset_team2 = [
+  {
+    question: "Password reset security question 1:\n\nWhat is your favourite fruit?",
+    answer: "durian"
+  },
+  {
+    question: "Password reset security question 2:\n\nWhat date were you born?",
+    answer: "17"
+  },
+  {
+    question: "Password reset security question 3:\n\nWhat do you like to *drink* out of cups??",
+    answer: "water",
+    message: "Part 2/3 of password has been reset: ***-na1a-***",
+  }
+]
+
+const reset_team3 = [
+  {
+    question: "Password reset security question 1:\n\nWhat is your favourite fruit?",
+    answer: "durian"
+  },
+  {
+    question: "Password reset security question 2:\n\nWhat date were you born?",
+    answer: "17"
+  },
+  {
+    question: "Password reset security question 3:\n\nWhat do you like to *drink* out of cups??",
+    answer: "water",
+    message: "Part 3/3 of password has been reset: ***-***-duri4n",
+  }
+]
